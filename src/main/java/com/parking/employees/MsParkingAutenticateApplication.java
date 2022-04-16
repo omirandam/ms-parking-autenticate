a90c67c3a3d85a525715dc8516473d7444e80c7a
omirandam <omirandam7@gmail.com>
2022-04-16 15:38:56 -0400

    first commit

---
+++ src/main/java/com/parking/employees/MsParkingAutenticateApplication.java
@@ -0,0 +1,13 @@
+package com.parking.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsParkingAutenticateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsParkingAutenticateApplication.class, args);
+	}
+
+}
